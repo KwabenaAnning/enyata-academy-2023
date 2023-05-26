88cb8a7aa5664935b8b38e83f96a8e901c810ded
Ghazi Anning <sammy.khobbie@gmail.com>
2023-05-26 08:15:45 +0000

    working directory

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,18 @@
+let  myName= "Ghazi";
+console.log(typeof myName)
+myName= Anning
+console.log(myName)
+
+let carName =
+{ axle: "front overheating",
+  BATTERY: "AC BATTERY",
+  Radiator: "Coolant",
+  Tyre: 225/60/15,
+  carClass: 'a',
+  color: "tourqoise",
+  engineType: "4 cylinders",
+  Braketype: "calliper",
+  Transmission: 'Automotic',
+  Gears: 6,
+
+}
